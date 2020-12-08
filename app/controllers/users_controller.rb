d00490340e0e43828360ed88149485f8eff991fc
suwa-chan <87.suwa.tetsuya@gmail.com>
2020-12-09 00:38:23 +0900

    ユーザーコントローラー作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "クックログへようこそ！"
+      flash[:success] = "cosonへようこそ！"
       redirect_to @user
     else
       render 'new'
