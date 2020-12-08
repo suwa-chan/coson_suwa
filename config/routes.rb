c4b7f8891a73f1121beba3bd661fd3d2a9cc53b4
suwa-chan <87.suwa.tetsuya@gmail.com>
2020-12-08 18:41:11 +0900

    静的ページルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/about'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
+  get :about,        to: 'static_pages#about'
 end
