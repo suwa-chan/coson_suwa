952d871ab66813b69011f8b0428bf8e969a0ec9b
suwa-chan <87.suwa.tetsuya@gmail.com>
2020-12-08 20:50:17 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get 'users/index'
   root 'static_pages#home'
   get :about,        to: 'static_pages#about'
+  get :signup,       to: 'users#new'
+  resources :users
 end
