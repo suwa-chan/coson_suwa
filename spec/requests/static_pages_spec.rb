240b13c759e4a0cb8527b2d105bf8e2fadfb7dfd
suwa-chan <87.suwa.tetsuya@gmail.com>
2020-12-08 20:43:11 +0900

    システムテスト

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,19 +1,34 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "静的ページ", type: :request do
+RSpec.describe "StaticPages", type: :system do
   describe "トップページ" do
-    it "正常なレスポンスを返すこと" do
-      get root_path
-      expect(response).to be_successful
-      expect(response).to have_http_status "200"
+    context "ページ全体" do
+      before do
+        visit root_path
+      end
+
+      it "cosonの文字列が存在することを確認" do
+        expect(page).to have_content 'coson'
+      end
+
+      it "正しいタイトルが表示されることを確認" do
+        expect(page).to have_title full_title
+      end
     end
   end
-  
-  describe "クックログとは？ページ" do
-    it "正常なレスポンスを返すこと" do
-      get about_path
-      expect(response).to be_successful
-      expect(response).to have_http_status "200"
+
+  describe "ヘルプページ" do
+    before do
+      visit about_path
+    end
+
+    it "cosonとは？の文字列が存在することを確認" do
+      expect(page).to have_content 'cosonとは？'
+    end
+
+    it "正しいタイトルが表示されることを確認" do
+      expect(page).to have_title full_title('cosonとは？')
     end
   end
+
 end
